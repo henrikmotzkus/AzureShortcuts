cf8c3b813a21b1432db9238bdd756d69a0ccaa88
Henrik Motzkus <hemotzku@microsoft.com>
2021-04-13 10:11:19 +0200

    docs updates

--- commandline.ps1
+++ commandline.ps1
@@ -1,8 +1,5 @@
 $ResourceGroupName = "ECE_NFS"
 
 
-New-AzResourceGroup -Name <re
 
-
-
-New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-customdata/azuredeploy.json
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateUri https://raw.githubusercontent.com/henrikmotzkus/NFS/main/template.json
