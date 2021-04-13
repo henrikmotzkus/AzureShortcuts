f71c0269da052d57d6d4a4de9caa6688ed4148cd
Henrik Motzkus <hemotzku@microsoft.com>
2021-04-13 13:21:29 +0200

    testing

--- commandline.ps1
+++ commandline.ps1
@@ -1,5 +1,11 @@
+Connect-AzAccount
+
 $ResourceGroupName = "ECE_NFS"
 
 
 
 New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateUri https://raw.githubusercontent.com/henrikmotzkus/NFS/main/template.json
+
+
+
+New-AzResourceGroupDeployment -ResourceGroupName ece_nfs -TemplateParameterUri https://raw.githubusercontent.com/henrikmotzkus/NFS/main/azuredeploy.parameters.json -TemplateUri https://raw.githubusercontent.com/henrikmotzkus/NFS/main/azuredeploy.json
